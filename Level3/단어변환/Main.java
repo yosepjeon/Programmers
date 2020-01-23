c4e5c6a4419fb57263e077e7314fd77d98334b27
yosepjeon <enekelx1@naver.com>
2020-01-23 20:11:04 +0900

    Level3 단어 변환 complete

--- Level3/단어변환/Main.java
+++ Level3/단어변환/Main.java
@@ -1,11 +1,24 @@
 package Level3.단어변환;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
 		
+		String begin = scr.next();
+		String target = scr.next();
+		int size = scr.nextInt();
+		String[] words = new String[size];
+		scr.nextLine();
+		
+		for(int i=0;i<size;i++) {
+			words[i] = scr.next();
+		}
+		
+		Solution solution = new Solution();
+		
+		System.out.println(solution.solution(begin, target, words));
 	}
 }
 
@@ -16,29 +29,64 @@ class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         
-        char[] beginChar = begin.toCharArray();
-        isVisited = new boolean[begin.length()];
+        isVisited = new boolean[words.length];
+        boolean isValidTarget = false;
         
+        for(String word : words) {
+        	if(word.equals(target))
+        		isValidTarget = true;
+        }
+        
+        if(!isValidTarget) {
+        	return 0;
+        }
+        
+        dfs(begin,target,words, 0);
+        answer = result;
         
         return answer;
     }
     
-    public void dfs(char[] begin,String target,String[] words) {
-    	if(String.valueOf(begin).equals(target)) {
-    		
+    public void dfs(String begin,String target,String[] words, int count) {
+    	if(result <= count) {
+    		return;
     	}
     	
-    	for(int i=0;i<words.length;i++) {
+    	char[] beginChar = begin.toCharArray();
+    	
+    	if(String.valueOf(begin).equals(target)) {
+    		if(result > count) {
+    			result = count;
+    			return;
+    		}
+    	}
+    	
+    	int beginCharSize = beginChar.length;
+    	int wordsSize = words.length;
+    	for(int i=0;i<wordsSize;i++) {
     		if(isVisited[i]) 
     			continue;
     		
-    		isVisited[i] = true;
-    		int count = 0;
     		char[] wordCharArr = words[i].toCharArray();
+    		int countMatch=0;
+    		int wordCharArrLen = wordCharArr.length;
     		
-    		for(int j=0;j<wordCharArr.length;j++) {
-    			
+    		for(int j=0;j<wordCharArrLen;j++) {
+    			if(beginChar[j] == wordCharArr[j]) {
+    				countMatch++;
+    			}
+    		}
+    		
+    		if(countMatch == beginCharSize-1) {
+    			isVisited[i] = true;
+    			dfs(words[i],target,words, count + 1);
+    			isVisited[i] = false;
     		}
     	}
     }
 }
+
+//hit
+//cog
+//6
+//hot dot dog lot log cog
