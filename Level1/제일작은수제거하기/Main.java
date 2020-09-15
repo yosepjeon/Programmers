75b5acc9ee558008fbeb494f2ceeaf164023bec5
yoggaebi <enekelx1@gmail.com>
2020-09-16 00:44:28 +0900

    제일 작은수 제거하기 complete

---
+++ Level1/제일작은수제거하기/Main.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+
+class Solution {
+	public int[] solution(int[] arr) {
+		int[] answer;
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		for(int n : arr) {
+			list.add(n);
+		}
+		
+		int min = Integer.MAX_VALUE;
+		int minIndex = -1;
+		for(int i =0 ;i<list.size();i++) {
+			if(min > list.get(i)) {
+				min = list.get(i);
+				minIndex = i;
+			}
+		}
+		
+		list.remove(minIndex);
+		answer= new int[list.size()];
+		
+        if(list.size() == 0) {
+            answer = new int[1];
+            answer[0] = -1;
+        }else {
+            for(int i = 0;i<answer.length;i++) {
+			answer[i] = list.get(i);
+		}   
+        }
+		
+		
+		return answer;
+	}
+}
--- skill_check_level1/다트게임/Practice2.java
+++ skill_check_level1/다트게임/Practice2.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 public class Practice2 {
 	public static void main(String[] args) {
-//		Scanner scr = new Scanner(System.in);
-//		String dartResult = scr.next();
-//		
-//		boolean numberCheck = false;
-		
-		System.out.println(Math.pow(2, 3));
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
 	}
 }
