085191ad46d6ca153a6e08d4dcfad488f031610d
yosepjeon <enekelx1@naver.com>
2022-01-11 23:46:43 +0900

    부족한금액 계산하기 complete

--- Level1/부족한금액계산하기/Main.java
+++ Level1/부족한금액계산하기/Main.java
@@ -1,4 +1,22 @@
 package Level1.부족한금액계산하기;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+
+        long totalPrice = 0;
+
+        for(int i=1;i<=count;i++) {
+            totalPrice += price * i;
+        }
+        answer = money - totalPrice;
+
+        return answer >= 0 ? 0 : answer * -1;
+    }
 }
