d842f2ea9462a6688d21034ffef8df52d57bb0a0
yosepjeon <enekelx1@naver.com>
2022-01-14 00:39:57 +0900

    나머지가 1이 되는 수 찾기 complete

---
+++ Level1/나머지가1이되는수찾기/Main.java
@@ -0,0 +1,24 @@
+package Level1.나머지가1이되는수찾기;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(12));
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=1;i<n;i++) {
+            if(n%i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
