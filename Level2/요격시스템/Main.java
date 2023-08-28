50fbee50e70b929c5329f93a5e5be9be58d60edf
YOSEP <enekelx1@naver.com>
2023-08-28 23:59:35 +0900

    요격시스템 complete

---
+++ Level2/요격시스템/Main.java
@@ -0,0 +1,19 @@
+package Level2.요격시스템;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
+
+class Solution {
+	public int solution(int[][] targets) {
+		int answer = 0;
+
+		int width = targets[0].length;
+		int height = targets.length;
+
+		return answer;
+	}
+}
